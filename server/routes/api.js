7acd48dfdb3e5d3e16fb7d84ca095836e9b22fe6
lazarks <sljivic.lazar037@gmail.com>
2022-01-25 18:22:47 +0100

    mondodb (mongoose) setup

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+const mongoose = require("mongoose");
+const db = "mongodb+srv://lazar037:lazar037pw@cluster0.xguqk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(db, (err) => {
+    if (err) {
+        console.error("Error!" + err);
+    } else {
+        console.log("Connected to mongodb");
+    }
+});
+
 router.get("/", (req, res) => {
     res.send("hello [api]");
 });
