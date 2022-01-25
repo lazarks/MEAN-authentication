87052f8dec40bb75de5f7ed6284705038fe86d53
lazarks <sljivic.lazar037@gmail.com>
2022-01-25 18:27:03 +0100

    registration api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require("../models/user");
+
 const mongoose = require("mongoose");
 const db = "mongodb+srv://lazar037:lazar037pw@cluster0.xguqk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose.connect(db, (err) => {
@@ -13,6 +15,18 @@ mongoose.connect(db, (err) => {
 
 router.get("/", (req, res) => {
     res.send("hello [api]");
+});
+
+router.post("/register", (req, res) => {
+    let userData = req.body;
+    let user = new User(userData);
+    user.save((error, registeredUser) => {
+        if (error) {
+            console.log(error);
+        } else {
+            res.status(200).send(registeredUser);
+        }
+    });
 });
 
 module.exports = router;
