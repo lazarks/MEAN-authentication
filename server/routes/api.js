90248e951e9e87d14db61dbfb127be5e3dfdeacc
lazarks <sljivic.lazar037@gmail.com>
2022-01-25 18:56:41 +0100

    login api

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const User = require("../models/user");
 
 const mongoose = require("mongoose");
-const db = "mongodb+srv://lazar037:lazar037pw@cluster0.xguqk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const db = "mongodb+srv://lazar037:lazar037pw@cluster0.xguqk.mongodb.net/eventsDB?retryWrites=true&w=majority";
 mongoose.connect(db, (err) => {
     if (err) {
         console.error("Error!" + err);
@@ -17,6 +17,7 @@ router.get("/", (req, res) => {
     res.send("hello [api]");
 });
 
+// registration api
 router.post("/register", (req, res) => {
     let userData = req.body;
     let user = new User(userData);
@@ -25,6 +26,25 @@ router.post("/register", (req, res) => {
             console.log(error);
         } else {
             res.status(200).send(registeredUser);
+        }
+    });
+});
+
+// login api
+router.post("/login", (req, res) => {
+    let userData = req.body;
+
+    User.findOne({ email: userData.email }, (err, user) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (!user) {
+                res.status(401).send("Invalid email");
+            } else if (user.password !== userData.password) {
+                res.status(401).send("Invalid password");
+            } else {
+                res.status(200).send(user);
+            }
         }
     });
 });
